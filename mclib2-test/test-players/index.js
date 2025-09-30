bb0016477b8a0b8111e704f3ecc66bce781d1ebf
theking90000 <martin.cogh@gmail.com>
2025-09-30 11:48:46 +0200

    Add bot testing with mineflayer

---
+++ mclib2-test/test-players/index.js
@@ -0,0 +1,34 @@
+const mineflayer = require("mineflayer");
+
+const botCount = 2;
+
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+for (let i = 0; i < botCount; i++) {
+    const bot = mineflayer.createBot({
+        host: "127.0.0.1",
+        port: 25565,
+        username: `TestBot${i + 1}`,
+        maxCatchupTicks: Number.MAX_SAFE_INTEGER,
+
+    });
+
+    console.log('Bot TestBot' + (i + 1) + ' created');
+
+    bot.on('kicked', console.log)
+    bot.on('error', console.log)
+
+    bot._client.on('chat', (chat) => {
+        console.log(`${bot.username} received chat: ${chat.message}`);
+    })
+
+    bot.on('spawn', async ()=>{
+        await wait(1000);
+
+        const block = bot.findBlock({matching:(b)=>true, maxDistance:5});
+       // console.log('block found:', block);
+        //bot.inte
+        await bot.dig(block);
+        console.log('interacted with a block');
+    })
+}
